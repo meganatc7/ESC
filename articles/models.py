92bc73b652a4a6be6039650634360235f5c93338
meganatc7 <meganatc7@gmail.com>
2021-04-06 23:59:52 +0900

    Add skeleton

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='like_articles',
+    )
+
+    def __str__(self):
+        return f'{self.title}: {self:content}'
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='like_comments',
+    )
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='Photo/%Y%m%d')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
