3ea8461dab3abd5476ccfc8e213c07ffea9d6c6a
daeungdaeung <kangdy0131@gmail.com>
2021-04-08 00:14:23 +0900

    mistake

--- forecasts/views.py
+++ forecasts/views.py
@@ -6,9 +6,9 @@ from pprint import pprint
 # Create your views here.
 @login_required
 def index(request):
-    my_serviceKey = '3urgccFNfwIp7ePyvIBfqtDLrK7Sxy2YZkHZ4lc33Cf%2F242KukfpnMSZ8wPOQCh716qplOd0Pp3AtewChHHfrg%3D%3D'
+    server_serviceKey = '3urgccFNfwIp7ePyvIBfqtDLrK7Sxy2YZkHZ4lc33Cf%2F242KukfpnMSZ8wPOQCh716qplOd0Pp3AtewChHHfrg%3D%3D'
     
-    vilage_fcst_url = f'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey={my_serviceKey}&dataType=JSON&base_date=20210408&base_time=0000&nx=58&ny=74'
+    vilage_fcst_url = f'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey={server_serviceKey}&dataType=JSON&base_date=20210408&base_time=0000&nx=58&ny=74'
     response = requests.get(vilage_fcst_url).json()
 
     pprint(response)
