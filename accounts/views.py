ecfaae35cd94d9806bf02fa810cc897e1dbe8752
meganatc7 <meganatc7@gmail.com>
2021-04-08 21:11:29 +0900

    Modify login feature

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
+@require_http_methods(['GET','POST'])
 def signup(request):
     # 로그인 되어있는 경우 인덱스 페이지로
     if request.user.is_authenticated:
@@ -26,12 +28,18 @@ def signup(request):
     return render(request, 'accounts/signup.html', context)
 
 
+@require_http_methods(['GET','POST'])
 def login(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    # POST 요청인 경우 유효성 검사한 뒤 로그인
+    if request.method == 'POST': 
         form = CustomAuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
             return redirect(request.GET.get('next') or 'articles:index')
+    # GET 요청인 경우 로그인 페이지로
     else:
         form = CustomAuthenticationForm()
     context = {
