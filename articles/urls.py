4a3f56774484f0e563b0967d32fc1850a18ce7c7
eunzi-kim <rladmswlek@naver.com>
2021-04-08 23:34:44 +0900

    Add comment create feature

--- articles/urls.py
+++ articles/urls.py
@@ -5,4 +5,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
+    path('<int:article_pk>/', views.detail, name='detail'),
+    # 댓글 작성
+    path('<int:article_pk>/comment', views.comment_create, name='comment_create'),
 ]
