9cd61bf2a3b25be8171263889ccca8bec3886836
eunzi-kim <rladmswlek@naver.com>
2021-04-11 17:01:41 +0900

    Add delete feature

--- articles/urls.py
+++ articles/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:article_pk>/', views.detail, name='detail'),
     path('<int:article_pk>/update/', views.update, name='update'),
+    path('<int:article_pk>/delete/', views.delete, name='delete'),
     # 댓글 작성
     path('<int:article_pk>/comment', views.comment_create, name='comment_create'),
 ]
