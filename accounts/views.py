af8e1e1d5ae9e75bcbe0948fc53fc08112064938
meganatc7 <meganatc7@gmail.com>
2021-04-18 19:06:24 +0900

    Add find password feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import CustomUserCreationForm, CustomAuthenticationForm, CustomUserChangeForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm, CustomUserChangeForm, CustomPasswordChangeForm, CustomPasswordResetForm, CustomSetPasswordForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, PasswordResetForm
 from django.views.decorators.http import require_http_methods, require_POST, require_safe
 from django.contrib.auth.decorators import login_required
 from django.core.mail import EmailMessage, send_mail
@@ -12,7 +12,9 @@ from .models import User
 from django.http import JsonResponse
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
+from django.contrib import messages
 import json
+import string, random
 
 # Create your views here.
 # 가입 유저에게 이메일 보내는 함수
@@ -77,6 +79,8 @@ def login(request):
         if form.is_valid():
             auth_login(request, form.get_user())
             return redirect(request.GET.get('next') or 'articles:index')
+        else:
+            messages.add_message(request, messages.WARNING, '아이디 혹은 비밀번호가 틀렸습니다.')
     # GET 요청인 경우 로그인 페이지로
     else:
         form = CustomAuthenticationForm()
@@ -117,6 +121,7 @@ def update(request):
     return render(request, 'accounts/update.html', context)
 
 
+@require_POST
 def updateprofile(request):
     if request.method == 'POST':
         user = User.objects.get(pk=request.user.pk)
@@ -132,6 +137,7 @@ def delete(request):
     return redirect('articles:index')
 
 
+@require_safe
 def profile(request, nickname):
     person = get_object_or_404(get_user_model(), nickname=nickname)
     form = CustomUserChangeForm()
@@ -140,3 +146,88 @@ def profile(request, nickname):
         'form': form,
     }
     return render(request, 'accounts/profile.html', context)
+
+
+@require_http_methods(['GET','POST'])
+def changepw(request):
+    if request.method == 'POST':
+        form = CustomPasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Success!!')
+            return redirect('accounts:profile', user.nickname)
+        else:
+            messages.error(request, 'Failed!!')
+    form = CustomPasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/password.html', context)
+
+
+@require_http_methods(['GET','POST'])
+def findpw(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        target_user = User.objects.get(email=email)
+        print(target_user.username)
+        if target_user:
+            # 임의의 인증번호 생성
+            string_pool = string.ascii_letters + string.digits
+            auth_num = ""
+            for _ in range(8):
+                auth_num += random.choice(string_pool)
+            
+            target_user.auth = auth_num
+            target_user.save()
+
+            send_mail(
+                '비밀번호 찾기 인증메일입니다.',
+                f'인증번호: {auth_num}',
+                'meganatc7@gmail.com',
+                [email],
+            )
+        return JsonResponse({'result': target_user.username})
+    else:
+        form = CustomPasswordResetForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/findpassword.html', context)
+
+
+def authpw(request):
+    user_id = request.POST.get('user_id')
+    input_auth_num = request.POST.get('input_auth_num')
+    target_user = User.objects.get(username=user_id, auth=input_auth_num)
+    target_user.auth = ""
+    target_user.save()
+    request.session['auth'] = target_user.username
+
+    return JsonResponse({'result': target_user.username})
+
+
+@require_http_methods(['GET','POST'])
+def resetpw(request):
+    if request.method == 'POST':
+        session_user = request.session['auth']
+        current_user = User.objects.get(username=session_user)
+        auth_login(request, current_user)
+
+        reset_password_form = CustomSetPasswordForm(request.user, request.POST)
+
+        if reset_password_form.is_valid():
+            user = reset_password_form.save()
+            messages.success(request, '비밀번호 변경 완료!')
+            auth_logout(request)
+            return redirect('accounts:login')
+        else:
+            auth_logout(request)
+            # request.session['auth'] = session_user
+    else:
+        reset_password_form = CustomSetPasswordForm(request.user)
+    context = {
+        'form': reset_password_form,
+    }
+    return render(request, 'accounts/password_reset.html', context)
