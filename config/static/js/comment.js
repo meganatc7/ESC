773344218afa5e6b10ed542d533ca1cdfff34edd
eunzi-kim <rladmswlek@naver.com>
2021-04-20 21:28:55 +0900

    Add comment update feature

--- config/static/js/comment.js
+++ config/static/js/comment.js
@@ -1,16 +1,18 @@
-const commentUpdeat = (id) => {
-  let commentUpdate = document.querySelector(`.commentUpdate${id}`);
-  let commentUpdateForm = document.querySelector(`.commentUpdateForm${id}`);
-  let commentContent = document.querySelector(`.commentContent${id}`);
-}
+// 1. 모든 수정 버튼을 갖고온다.
+const updateBtns = document.querySelectorAll('.commentUpdateBtn')
+// ['1번 댓글 수정버튼', '2번 ...', '3번 ...', ...]
 
-$(document).ready(function(){
-  $("#commentUpdate").click(function(){
-    $("#commentUpdateForm").toggle();
-    $("#commentContent").toggle();
-  });
-  $("#comment_update_cancel").click(function(){
-    $("#comment_content").show();
-    $("#comment_update_form").hide();
-  });
-});
+// 2. 모든 수정 버튼에 "클릭 이벤트를 단다."
+updateBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // 3. 기존의 내용을 지우고,
+    console.log("success")
+    console.log()
+    console.log(document.getElementById("#commentContent{{ updateBtns.dataset.id }}"))
+    // document.getElementById("#commentContent${updateBtns.dataset.id}").style.display = "none";
+    // 4. 수정 form을 보여준다.
+    
+    // document.getElementById("#commentUpdateForm${updateBtns.dataset.id}").style.display = "block";
+  })
+})
+// updateBtns.dataset.id == comment.id
